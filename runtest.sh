947986b8e8876e36d42d25599a6c4bae30eca1e2
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-13 15:06:30 +0100

    nicer output

--- runtest.sh
+++ runtest.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 echo starting container
 docker run -d -p5000 -e MONGO_URL=mongodb://scylla:30017 smurve/capsnet-fashion:test
-echo done. Getting container id.
+echo done. 
+echo Getting container id.
 c=$(docker ps | grep "smurve/capsnet-fashion:test" | awk '{print $1}')
-echo done: $c. Getting port
+echo done. Container id is $c. 
+echo Getting port
 port=$(docker inspect --format "{{.NetworkSettings.Ports}}" $c | awk '{print $2}' | cut -d "}" -f1)
-echo waiting 5s
-sleep 5
-echo done: $port. Curling
+echo done: Port number is $port. 
+echo curling after 2 seconds
+sleep 2
 res=$(curl localhost:$port | grep 'Author')
-[ "$res" != "" ] || echo Failed. Result: $res 
-echo done: $res. Stopping container
+echo "result was '$res'" 
+[ "$res" != "" ] || echo Failure!! 
+[ "$res" != "" ] && echo Sucess!!
+
+echo done testing. Stopping container
 docker stop $c
-echo done. $removing container
+echo done. Removing container
 docker rm $c
+echo done. Reporting back
 [ "$res" != "" ] 
