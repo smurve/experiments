4c4dd1c0caf8c9e31c9fa3583b403182288de397
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-14 18:17:23 +0100

    Execution within container? Testing it.

--- batchers/test_mnistBatcher.py
+++ batchers/test_mnistBatcher.py
@@ -5,4 +5,4 @@ class TestMnistBatcher(object):
     def test_extract_data(self):
         m = MnistBatcher('./testdata/t10k-images-idx3-ubyte.gz', 30)
         assert m.data.shape == (30, 28, 28, 1)
-        assert m.data[0, 0, 0, 0] == -0.6
+        assert m.data[0, 0, 0, 0] == -0.5
