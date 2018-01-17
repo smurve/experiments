5fc8babdbf74db814350c5e3c59857598547f444
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-17 03:08:08 +0100

    Trainer Script and 3-Layer Feedforward -> 98%, 99.8%

---
+++ model_trainer.py
@@ -0,0 +1,52 @@
+# from models.model import TensorflowMnistSoftmaxRegression
+from models.model import TensorflowHiddenx2Feedforward
+from batchers import MnistBatcher
+import tensorflow as tf
+import os
+
+
+SEED = 123
+MODEL_FILE = os.path.join(os.environ.get("TMPDIR", "./"), "model.cpkt")
+SCOPE = "mnist_softmax_regression"
+DATA_DIR = "/var/ellie/data/mnist"
+img_train = os.path.join(DATA_DIR, "train-images-idx3-ubyte.gz")
+lbl_train = os.path.join(DATA_DIR, "train-labels-idx1-ubyte.gz")
+img_test = os.path.join(DATA_DIR, "t10k-images-idx3-ubyte.gz")
+lbl_test = os.path.join(DATA_DIR, "t10k-labels-idx1-ubyte.gz")
+train_batcher = MnistBatcher(img_file=img_train, lbl_file=lbl_train, num_samples=60000)
+test_batcher = MnistBatcher(img_file=img_test, lbl_file=lbl_test, num_samples=10000)
+NUM_EPOCHS = 300
+BATCH_SIZE = 1000
+# model = TensorflowMnistSoftmaxRegression(SCOPE, SEED)
+model = TensorflowHiddenx2Feedforward(SCOPE, SEED)
+
+with tf.Session() as sess:
+    train_step = tf.train.AdamOptimizer().minimize(model.objective)
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(NUM_EPOCHS):
+        train_batcher.reset()
+        if (epoch % 10) == 0:
+            print("Epoch Nr. %s" % (epoch + 1))
+            test_batcher.reset()
+            test_images, test_labels = test_batcher.next_batch(10000)
+            test_images = test_images.reshape(-1, 784)
+            test_accuracy = sess.run(model.accuracy,
+                                     feed_dict={model.images: test_images, model.labels: test_labels})
+            test_accuracy = round(test_accuracy * 100, 2)
+            print("Test Accuracy: %s" % test_accuracy)
+
+            train_images, train_labels = train_batcher.next_batch(10000)
+            train_images = train_images.reshape(-1, 784)
+            train_accuracy = sess.run(model.accuracy,
+                                      feed_dict={model.images: train_images, model.labels: train_labels})
+            train_accuracy = round(train_accuracy * 100, 2)
+            print("Train Accuracy: %s" % train_accuracy)
+            train_batcher.reset()
+
+        while train_batcher.has_more():
+            images, labels = train_batcher.next_batch(BATCH_SIZE)
+            images = images.reshape(-1, 784)
+            train_step.run(session=sess, feed_dict={model.images: images, model.labels: labels})
+
+    saver = tf.train.Saver()
+    saver.save(sess, MODEL_FILE)
--- models/model.py
+++ models/model.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 
 
 class TensorflowMnistSoftmaxRegression:
-    """
-    A Model encapsulating
-    """
 
     def __init__(self, scope_name, seed):
         """
@@ -22,7 +19,54 @@ class TensorflowMnistSoftmaxRegression:
             a = tf.nn.softmax(z, name="activation")
             objective = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=a),
                                        name="objective")
+
+            correct_prediction = tf.equal(tf.argmax(a, 1), tf.argmax(y_, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
         self.images = images
         self.labels = labels
         self.activation = a
         self.objective = objective
+        self.accuracy = accuracy
+
+
+class TensorflowHiddenx2Feedforward:
+
+    def __init__(self, scope_name, seed):
+        """
+        :param scope_name: name for this graph
+        :param seed: A random integer seed for the weight initialization
+        """
+        with tf.name_scope(scope_name):
+            images = tf.placeholder(tf.float32, shape=[None, 784], name="images")
+            labels = tf.placeholder(tf.int32, shape=[None], name="labels")
+            y_ = tf.one_hot(labels, 10)
+            rnd_w1 = tf.truncated_normal([784, 256], mean=0.0, stddev=0.1, dtype=tf.float32, seed=seed)
+            w1 = tf.Variable(rnd_w1, name="w1")
+            b1 = tf.Variable(tf.zeros([256]), dtype=tf.float32, name="b1")
+            z1 = tf.matmul(images, w1) + b1
+            a1 = tf.nn.relu(z1, name="activation1")
+
+            rnd_w2 = tf.truncated_normal([256, 64], mean=0.0, stddev=0.1, dtype=tf.float32, seed=seed)
+            w2 = tf.Variable(rnd_w2, name="w2")
+            b2 = tf.Variable(tf.zeros([64]), dtype=tf.float32, name="b2")
+            z2 = tf.matmul(a1, w2) + b2
+            a2 = tf.nn.softmax(z2, name="activation2")
+
+            rnd_w3 = tf.truncated_normal([64, 10], mean=0.0, stddev=0.1, dtype=tf.float32, seed=seed)
+            w3 = tf.Variable(rnd_w3, name="w3")
+            b3 = tf.Variable(tf.zeros([10]), dtype=tf.float32, name="b3")
+            z3 = tf.matmul(a2, w3) + b3
+            a3 = tf.nn.relu(z3, name="activation3")
+
+            objective = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=a3),
+                                       name="objective")
+
+            correct_prediction = tf.equal(tf.argmax(a3, 1), tf.argmax(y_, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        self.images = images
+        self.labels = labels
+        self.activation = a3
+        self.objective = objective
+        self.accuracy = accuracy
