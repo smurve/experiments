879eff5280e43db4b4e2df988db6c607aed62ab2
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-20 14:18:08 +0100

    model trainer now cleaning up logdir before starting the training

--- model_trainer.py
+++ model_trainer.py
@@ -7,9 +7,21 @@ from models.particular import MODEL_FILE
 from models.trainer import Params, train
 from mnist import train_batcher, test_batcher
 import tensorflow as tf
+import os
 
 train_batcher.set_preprocessor(lambda x: x.reshape(-1, 784))
 test_batcher.set_preprocessor(lambda x: x.reshape(-1, 784))
+
+
+def clean_tensorboard_logs(logdir):
+    for subdir in ['test', 'train']:
+        path = os.path.join(logdir, subdir)
+        if os.path.isdir(path):
+            files = os.listdir(path)
+            for file in files:
+                fpath = os.path.join(path, file)
+                os.unlink(fpath)
+
 
 config = tf.ConfigProto(
     log_device_placement=False,
@@ -17,11 +29,15 @@ config = tf.ConfigProto(
     intra_op_parallelism_threads=12,
     inter_op_parallelism_threads=12)
 
+LOGDIR = "/var/ellie/models/mnist"
+
 params = Params(num_epochs=50,
                 batch_size=2000,
                 test_batch_size=1000,
                 model_file=MODEL_FILE,
-                log_dir="/var/ellie/models/mnist")
+                log_dir=LOGDIR)
+
+clean_tensorboard_logs(LOGDIR)
 
 model = dense_4_layers()
 train(model, train_batcher, test_batcher, config, params)
