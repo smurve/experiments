f85ded46f4ae348a05befbdc5707d67b3dad32b3
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-21 12:24:41 +0100

    new location of training_job.yml

--- shell/start_training_job.sh
+++ shell/start_training_job.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 status=$(kubectl get pods --show-all --no-headers --selector=job-name=capsnet-fashion-trainer | head -n 1 | awk '{print $3}')
 
-kubectl delete -f training_job.yml || echo "INFO: Couldn't delete. That's OK. Probably was deleted before"
-kubectl create -f training_job.yml
+kubectl delete -f k8s/training_job.yml || echo "INFO: Couldn't delete. That's OK. Probably was deleted before"
+kubectl create -f k8s/training_job.yml
