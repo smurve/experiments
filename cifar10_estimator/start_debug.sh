badd5420f308644245dc9aa2f3428dac4c4bdbfd
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-23 14:17:08 +0100

    Very first careful refactorings to start abstracting the valuable tower orchestration from the cifar model specifics

--- cifar10_estimator/start_debug.sh
+++ cifar10_estimator/start_debug.sh
@@ -2,13 +2,20 @@
 
 echo "================================================================"
 echo
+echo    "Warning: This is a DEBUG version using local directories only.."
+echo
 echo    "WARNING: Not using GPUs if you don't specify --num-gpus=<num>"
 echo
 echo "================================================================"
 
+ROOT_DIR=/Users/wgiersche/tmp
+
 python cifar10_main.py \
-    --job-dir=/var/ellie/models/cifar10_new \
+    --job-dir=${ROOT_DIR}/cifar10_new \
     --data-dir=/var/ellie/data/cifar10_tfr/ \
     --use-distortion-for-training=false \
-    "$@"
+    --train-steps=100 \
+    --train-batch-size=2000 \
+    --eval-batch-size=2000 \
+    --data-format=channels_last
 
