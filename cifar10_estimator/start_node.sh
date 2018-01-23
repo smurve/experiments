1c9b68835dddce233d9fac1f47683433b7f2ffd0
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-23 10:49:32 +0100

    using sync and passing additional params through

--- cifar10_estimator/start_node.sh
+++ cifar10_estimator/start_node.sh
@@ -9,7 +9,7 @@ shift
 
 export TF_CONFIG=$(python tf_config.py ${node_type} ${task_index})
 
-echo python cifar10_main.py \
+python cifar10_main.py \
     --job-dir=/var/ellie/models/cifar10_new \
     --data-dir=/var/ellie/data/cifar10_tfr/ \
     --use-distortion-for-training=false \
