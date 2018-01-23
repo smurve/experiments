7c4e8fd07af6836161e98ed50b6682461b98380e
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-23 10:48:07 +0100

    using sync and passing additional params through

--- cifar10_estimator/start_node.sh
+++ cifar10_estimator/start_node.sh
@@ -3,13 +3,17 @@
 node_type=$1 # either of ps, master, worker
 task_index=$2
 steps=$3
+shift
+shift
+shift
 
 export TF_CONFIG=$(python tf_config.py ${node_type} ${task_index})
 
-python cifar10_main.py \
+echo python cifar10_main.py \
     --job-dir=/var/ellie/models/cifar10_new \
     --data-dir=/var/ellie/data/cifar10_tfr/ \
     --use-distortion-for-training=false \
     --num-gpus=2 \
-    --train-steps=${steps}
-
+    --sync \
+    --train-steps=${steps} \
+    "$@"
