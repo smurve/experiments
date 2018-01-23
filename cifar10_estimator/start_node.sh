5dfa1febaa35c8ca4364da7f485b09e6cb9fcb57
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-23 10:01:08 +0100

    convenience scripts for multi-node clusters

---
+++ cifar10_estimator/start_node.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+node_type=$1 # either of ps, master, worker
+task_index=$2
+
+export TF_CONFIG=$(python tf_config.py ${node_type} ${task_index})
+
+python cifar10_main.py \
+    --job-dir=/var/ellie/models/cifar10_new \
+    --data-dir=/var/ellie/data/cifar10_tfr/ \
+    --use-distortion-for-training=false \
+    --num-gpus=2 \
+    --train-steps=5000 \
+    --num_workers=2
