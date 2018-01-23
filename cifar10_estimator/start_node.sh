8144385f9eeb2af81092005b550b054343fa70f3
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-23 10:27:50 +0100

    number of steps as cmd line argument

--- cifar10_estimator/start_node.sh
+++ cifar10_estimator/start_node.sh
@@ -2,6 +2,7 @@
 
 node_type=$1 # either of ps, master, worker
 task_index=$2
+steps=$3
 
 export TF_CONFIG=$(python tf_config.py ${node_type} ${task_index})
 
@@ -10,5 +11,5 @@ python cifar10_main.py \
     --data-dir=/var/ellie/data/cifar10_tfr/ \
     --use-distortion-for-training=false \
     --num-gpus=2 \
-    --train-steps=5000
+    --train-steps=${steps}
 
