9247f8fb44c3e26729b4b1d431f435b169022e76
Wolfgang Giersche <wgiersche@gmail.com>
2018-02-04 17:20:17 +0100

    back to the old experiment API. New API gets stuck loading from checkpoint

--- cifar10_estimator/start_debug.sh
+++ cifar10_estimator/start_debug.sh
@@ -11,7 +11,7 @@ echo "================================================================"
 ROOT_DIR=/Users/wgiersche/tmp
 
 
-python cifar10_new_resnet_main.py \
+python cifar10_with_resnet_main.py \
     --job-dir=${ROOT_DIR}/cifar10_new \
     --data-dir=/var/ellie/data/cifar10_tfr/ \
     --use-distortion-for-training=false \
