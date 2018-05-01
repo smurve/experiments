c96a6c3abe8e74106033833efe04f43f421bafc6
Wolfgang Giersche <wgiersche@gmail.com>
2018-05-01 07:04:25 +0200

    multi-gpu externalized to trainer setup (softmax_cross_entropy_trainer.py)

--- experiments/helpers/softmax_cross_entropy_trainer.py
+++ experiments/helpers/softmax_cross_entropy_trainer.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+from helpers.gpu_utils import validate_batch_size_for_multi_gpu
 
 
-def create_model_fn(model_factory, optimizer, dict_key=None):
+def create_model_fn(model_factory, optimizer, options, dict_key=None):
     """
     Create the model_fn to hand to the Estimator
     :param model_factory: A function that takes the model_fn params to create the model tensor
     :param dict_key: optional key to lookup the input tensor if 'features' is a dictionary
+    :param options: an object specifying batch_size and multi_gpu usage
     :param optimizer: the optimizer to use for training, ignored if not running in training mode
     :return:
     """
@@ -19,12 +21,19 @@ def create_model_fn(model_factory, optimizer, dict_key=None):
         :param params: any params
         :return: an appropriate EstimatorSpec for the chosen mode.
         """
+
+        if options.multi_gpu:
+            validate_batch_size_for_multi_gpu(options.batch_size)
+
         input_tensor = features
         if isinstance(input_tensor, dict):
             input_tensor = features[dict_key]
 
         model = model_factory(params)
 
+        ######################################################################
+        #      Spec for Prediction
+        ######################################################################
         if mode == tf.estimator.ModeKeys.PREDICT:
             logits = model(input_tensor, training=False)
             predictions = {
@@ -38,12 +47,14 @@ def create_model_fn(model_factory, optimizer, dict_key=None):
                     'classify': tf.estimator.export.PredictOutput(predictions)
                 })
 
+        ######################################################################
+        #      Spec for Training
+        ######################################################################
         if mode == tf.estimator.ModeKeys.TRAIN:
 
-            the_optimizer = optimizer
             # If we are running multi-GPU, we need to wrap the optimizer.
-            if params.get('multi_gpu'):
-                the_optimizer = tf.contrib.estimator.TowerOptimizer(optimizer)
+            the_optimizer = optimizer if not options.multi_gpu else \
+                tf.contrib.estimator.TowerOptimizer(optimizer)
 
             logits = model(input_tensor, training=True)
             loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
@@ -53,14 +64,19 @@ def create_model_fn(model_factory, optimizer, dict_key=None):
             # LoggingTensorHook.
             tf.identity(accuracy[1], name='train_accuracy')
             tf.summary.scalar('train_accuracy', accuracy[1])
+
             return tf.estimator.EstimatorSpec(
                 mode=tf.estimator.ModeKeys.TRAIN,
                 loss=loss,
                 train_op=the_optimizer.minimize(loss, tf.train.get_or_create_global_step()))
 
+        ######################################################################
+        #      Spec for Evaluation
+        ######################################################################
         if mode == tf.estimator.ModeKeys.EVAL:
             logits = model(input_tensor, training=False)
             loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
             return tf.estimator.EstimatorSpec(
                 mode=tf.estimator.ModeKeys.EVAL,
                 loss=loss,
@@ -70,4 +86,11 @@ def create_model_fn(model_factory, optimizer, dict_key=None):
                             labels=labels,
                             predictions=tf.argmax(logits, axis=1)),
                 })
-    return _model_fn
+        
+    ######################################################################
+    #      If multi-gpu, wrap into map-reduce replicator
+    ######################################################################
+    return _model_fn if not options.multi_gpu else tf.contrib.estimator.replicate_model_fn(
+        _model_fn, loss_reduction=tf.losses.Reduction.MEAN)
+
+
--- experiments/mnist_sota/models/dense3.py
+++ experiments/mnist_sota/models/dense3.py
@@ -8,17 +8,23 @@ class Model:
     """
 
     # noinspection PyUnusedLocal
-    def __init__(self, params):
-        """Constructor
+    def __init__(self, params, inp=784, hidden1=1024, hidden2=128, output=10):
+        """
+        Constructor: produces three dense layer functions with dimensions
+        input->hidden1, hidden1->hidden2, hidden2->output
         Args:
-            params: not used here
+            params: not used in this simple model.
+            inp: dimension of the input layer
+            hidden1: dimension of the 1st hidden layer
+            hidden2: dimension of the 2nd hidden layer
+            output: dimension of the output layer
         """
 
-        self._input_shape = [-1, 784]
+        self._input_shape = [-1, inp]
 
-        self.fc1 = tf.layers.Dense(1024, activation=tf.nn.relu)
-        self.fc2 = tf.layers.Dense(128, activation=tf.nn.relu)
-        self.fc3 = tf.layers.Dense(10)
+        self.fc1 = tf.layers.Dense(hidden1, activation=tf.nn.relu)
+        self.fc2 = tf.layers.Dense(hidden2, activation=tf.nn.relu)
+        self.fc3 = tf.layers.Dense(output)
 
     def __call__(self, inputs, training):
         """Add operations to classify a batch of input images.
